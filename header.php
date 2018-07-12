f01c4dbe8d77f1395076e98ef7b67f174c37a523
Nosferya <clement.polito@gmail.com>
2018-07-12 10:43:18 +0200

    maj

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+  <div class="topnav" id="myTopnav">
+    <a href="#home" class="active">Accueil</a>
+    <a href="#news">Informations</a>
+    <a href="#contact">Films</a>
+    <a href="#about" class="floatR"><i class="fas fa-user"></i></a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+  <i class="fa fa-bars"></i>
+    </a>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -12,18 +12,7 @@
 </head>
 
 <body>
-  <header>
-    <div class="topnav" id="myTopnav">
-      <a href="#home" class="active">Accueil</a>
-      <a href="#news">Informations</a>
-      <a href="#contact">Films</a>
-      <a href="#about" class="floatR"><i class="fas fa-user"></i></a>
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-      </a>
-    </div>
-  </header>
-
+<?php include("header.php") ?>
   <main>
 
     <div id="demo" class="carousel slide" data-ride="carousel">
